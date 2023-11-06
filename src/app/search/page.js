f5d6751add38329a929b0fc1589bb8fad7ce6c32
David Wijaya <davidwijaya0801@gmail.com>
2023-11-06 22:48:56 +0700

    feat: ui frontend

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,3 +1,15 @@
-export default function page() {
-    return <div> SEARCH PAGE </div>
+// using function
+// export default function page() {
+//     return <div> SEARCH PAGE </div>
+// }
+
+const Page = () => {
+    return (
+        <>
+            <h1>SEARCH PAGE ARROW FUNCTION </h1>
+            <p>ini adalah search</p>
+        </>
+    )
 }
+
+export default Page
