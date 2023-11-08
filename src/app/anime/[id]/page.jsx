3c9a079259934504d2edb506a07ff232b3b1d483
David Wijaya <davidwijaya0801@gmail.com>
2023-11-08 17:29:22 +0700

    feat: anime detail content

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,16 +1,46 @@
-import { getAnimeResponse } from "@/app/libs/api-libs"
+import { getAnimeResponse } from "@/app/libs/api-libs";
+import Image from "next/image";
 
 const Page = async ({ params: { id } }) => {
-    const anime = await getAnimeResponse(`anime/${id}`)
-    console.log(anime);
+    const anime = await getAnimeResponse(`anime/${id}`);
 
     return (
         <>
             <div className="pt-4 px-4">
-                <h3 className="text-2xl text-color-primary">ANIME ID DETAIL</h3>
+                <h3 className="text-2xl text-color-accent">
+                    {anime.data.title} - {anime.data.year}
+                </h3>
+            </div>
+            <div className="pt-4 px-4 flex gap-2 text-color-accent overflow-x-auto">
+                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-accent p-2">
+                    <h3>Peringkat</h3>
+                    <p>{anime.data.rank}</p>
+                </div>
+                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-accent p-2">
+                    <h3>Score</h3>
+                    <p>{anime.data.score}</p>
+                </div>
+                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-accent p-2">
+                    <h3>Anggota</h3>
+                    <p>{anime.data.members}</p>
+                </div>
+                <div className="w-36 flex flex-col justify-center items-center rounded border border-color-accent p-2">
+                    <h3>Episodes</h3>
+                    <p>{anime.data.episodes}</p>
+                </div>
+            </div>
+            <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-accent">
+                <Image
+                    src={anime.data.images.webp.image_url}
+                    alt={anime.data.images.jpg.image_url}
+                    width={250}
+                    height={250}
+                    className="w-full rounded object-cover"
+                />
+                <p className="text-justify text-xl">{anime.data.synopsis}</p>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Page
+export default Page;
